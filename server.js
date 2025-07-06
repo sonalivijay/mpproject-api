e9d23917a6d772141e014292b2ffeaaa7c46aa21
Sonali <sonalivijaywargiya@gmial.com>
2025-07-06 14:28:29 +0530

    add credentials for mysql

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ✅ for form data
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
